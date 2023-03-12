5c27f2323014046bba1c154611bbc9f5ef5301c8
daniyelkinzhaliyev <daniyelkinzhaliyev@gmail.com>
2023-03-12 14:23:05 +0800

    First steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Header />
+    <h1>Hello world</h1>
     </div>
-  );
+  )
+}
+
+const Header = () => {
+  return(
+    <div>
+      <a href=''>Home</a>
+      <a href=''>Gallery</a>
+      <a href=''>About us</a>
+    </div>
+
+
+  )
+  
 }
 
 export default App;
